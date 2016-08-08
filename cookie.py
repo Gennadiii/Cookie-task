8566ed0abe3abbbdc755a199149616d1d30c2272
Gennadii <g.mishchevskii@gmail.com>
2016-08-08 18:20:34 +0300

    Update cookie.py

--- cookie.py
+++ cookie.py
@@ -85,20 +85,26 @@ if __name__ == '__main__':
         invalid_input = False
         data = json_load(log)
         inserted_money = input('\n'*2 +'Insert money:                    ')
-        if inserted_money[0] == 'e':
-            data['mistakes'] += 1
-            inserted_money = inserted_money[1:]
+        if len(inserted_money) == 0: print( "You've gotta put something..." )
         chosen_cookie = input('Choose the price of your cookie: ')
-        if not validated('\n' + 'You paid          ', inserted_money):
-            invalid_input = True
-        if not invalid_input and not validated('Your cookie costs ', chosen_cookie):
-            invalid_input = True
-        if invalid_input:
+        if len(inserted_money) == 0: print( "You've gotta choose something..." )
+        try:
+            if inserted_money[0] == 'e':
+                data['mistakes'] += 1
+                inserted_money = inserted_money[1:]
+            if not validated('\n' + 'You paid          ', inserted_money):
+                invalid_input = True
+            if not invalid_input and not validated('Your cookie costs ', chosen_cookie):
+                invalid_input = True
+            if invalid_input:
+                result = invalid_input_message
+            if not invalid_input: result = game(inserted_money, chosen_cookie)
+            print(result)
+            if ( len(inserted_money) != 0 and len(chosen_cookie) != 0 ) and ( inserted_money[0] == '-' or chosen_cookie[0] == '-' ): \
+            print('What are you doing? It\'s a real vending machine. You can\'t insert less than 0 money or order cheaper than 0 cookie ) But nice try ;))')
+        except:
             result = invalid_input_message
-        if not invalid_input: result = game(inserted_money, chosen_cookie)
-        print(result)
-        if ( len(inserted_money) != 0 and len(chosen_cookie) != 0 ) and ( inserted_money[0] == '-' or chosen_cookie[0] == '-' ): \
-        print('What are you doing? It\'s a real vending machine. You can\'t insert less than 0 money or order cheaper than 0 cookie ) But nice try ;))')
+            invalid_input = True
         data[ 'try_number_' + str( data['number_of_tries'] + 1 ) ] = [ inserted_money, chosen_cookie, result, invalid_input ]
         data['number_of_tries'] += 1
         data['reader_is_updated'] = False
